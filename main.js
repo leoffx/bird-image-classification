555c4ff951b19bbe933876bb4e1170f91081d0a6
Leonardo Ferreira <leonardoff.eng@gmail.com>
2024-06-06 10:35:17 +0200

    tidying

--- main.js
+++ main.js
@@ -1,113 +1,93 @@
-const inputButton = document.getElementById('input');
-const imageFrame = document.getElementById('image');
-const imageDiv = document.getElementById('imageDiv');
-const predictionFrame = document.getElementById('predictions');
-let model, image;
-let suggestedSpecies = [];
+async function main() {
+  const predictionFrame = document.getElementById("predictions");
+  const inputFrame = document.getElementById("input");
+  const imageFrame = document.getElementById("image");
 
-async function loadModels() {
-  const MODEL_URL =
-    'https://raw.githubusercontent.com/leoffx/tori-birds-identification/master/src/assets/tf/model.json';
-  model = await tf.loadGraphModel(MODEL_URL);
+  const [model, mapping] = await Promise.all([loadModel(), loadMapping()]);
   predictionFrame.innerText =
-    'Model loaded! Please upload a brazilian bird picture.';
-}
+    "Model loaded! Please upload a brazilian bird picture.";
 
-async function loadImage(input) {
-  var reader = new FileReader();
-  reader.readAsDataURL(input.files[0]);
-  reader.onload = function (e) {
-    imageFrame.src = e.target.result;
-  };
-}
-
-imageFrame.onload = function (e) {
-  predict().then((res) => {
-    predictionFrame.innerText = '';
-    const { values, indices } = tf.topk(res, 5);
-    const indicesArray = indices.arraySync()[0];
-    const valuesArray = values.arraySync()[0];
-    indicesArray.forEach((index, i) => {
-      const bird = birdDictionary[index];
-      const suggestionData = {
-        index,
-        confidence: Math.floor(valuesArray[i] * 100),
-        id: bird.id,
-        name: bird.name,
-        age: bird.age,
-        sex: bird.sex,
-      };
-      if (suggestionData.confidence > 10) {
-        birdName =
-          suggestionData.name.charAt(0).toUpperCase() +
-          suggestionData.name.slice(1);
-        switch (suggestionData.sex) {
-          case 'F':
-            birdSex = 'Female';
-            break;
-          case 'M':
-            birdSex = 'Male';
-            break;
-          default:
-            birdSex = '';
-        }
-        switch (suggestionData.age) {
-          case 'A':
-            birdAge = 'Adult';
-            break;
-          case 'J':
-            birdAge = 'Young';
-            break;
-          default:
-            birdAge = '';
-        }
-
-        suggestedSpecies.push(suggestionData);
-        var item = document.createElement('li');
-        item.appendChild(
-          document.createTextNode(
-            `${birdName}, ${birdAge} ${birdSex} : ${suggestionData.confidence}%`
-          )
-        );
-        predictionFrame.appendChild(item);
-      }
-    });
+  inputFrame.addEventListener("change", (event) => loadImage(event));
+  imageFrame.addEventListener("load", () => {
+    predictionFrame.innerText = "";
+    renderPredictions(model, mapping, imageFrame);
   });
-};
+}
 
-async function predict() {
-  while (!model) {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-  }
+async function loadModel() {
+  const MODEL_URL =
+    "https://raw.githubusercontent.com/leoffx/tori-birds-identification/master/src/assets/tf/model.json";
+  return tf.loadGraphModel(MODEL_URL);
+}
+async function loadMapping() {
+  const MAPPING_URL =
+    "https://raw.githubusercontent.com/leoffx/tori-birds-identification/master/src/assets/tf/mappingPredictionToObject.json";
+  const response = await fetch(MAPPING_URL);
+  return response.json();
+}
+
+async function loadImage(event) {
+  const reader = new FileReader();
+  reader.readAsDataURL(event.target.files[0]);
+  reader.addEventListener("load", () => {
+    const imageFrame = document.getElementById("image");
+    imageFrame.src = reader.result;
+  });
+}
+
+async function renderPredictions(model, mapping, imageFrame) {
+  const res = await predict(model, imageFrame);
+  const { values, indices } = tf.topk(res, 5);
+  const indicesArray = indices.arraySync()[0];
+  const valuesArray = values.arraySync()[0];
+  indicesArray.forEach((index, i) => {
+    const bird = mapping[index];
+    const suggestionData = {
+      index,
+      confidence: Math.floor(valuesArray[i] * 100),
+      id: bird.id,
+      name: bird.name,
+      age: bird.age,
+      sex: bird.sex,
+    };
+    birdName =
+      suggestionData.name.charAt(0).toUpperCase() +
+      suggestionData.name.slice(1);
+    switch (suggestionData.sex) {
+      case "F":
+        birdSex = "Female";
+        break;
+      case "M":
+        birdSex = "Male";
+        break;
+      default:
+        birdSex = "";
+    }
+    switch (suggestionData.age) {
+      case "A":
+        birdAge = "Adult";
+        break;
+      case "J":
+        birdAge = "Young";
+        break;
+      default:
+        birdAge = "";
+    }
+
+    const item = document.createElement("li");
+    item.textContent = `${birdName}, ${birdAge} ${birdSex} : ${suggestionData.confidence}%`;
+    const predictionFrame = document.getElementById("predictions");
+    predictionFrame.appendChild(item);
+  });
+}
+
+async function predict(model, imageFrame) {
   image = tf.browser
     .fromPixels(imageFrame)
     .resizeBilinear([224, 224])
-    .asType('float32');
+    .asType("float32");
   const normalizedImage = image.div([255]).expandDims();
-  prediction = await model.predict(normalizedImage);
-  return prediction;
+  return model.predict(normalizedImage);
 }
 
-function loadJSON(callback) {
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType('application/json');
-  xobj.open(
-    'GET',
-    'https://raw.githubusercontent.com/leoffx/tori-birds-identification/master/src/assets/tf/mappingPredictionToObject.json',
-    true
-  );
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == '200') {
-      callback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
-}
-
-function main() {
-  loadModels();
-  loadJSON((response) => {
-    birdDictionary = JSON.parse(response);
-  });
-}
 main();
